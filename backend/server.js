0ca44751bedb483da516ec3a70be6c51e0f2708f
Thigulla Chatrapathi Reddy <chatrapathireddythigulla@gmail.com>
2026-03-24 20:47:25 +0530

    render frontend 3rd try

--- backend/server.js
+++ backend/server.js
@@ -298,7 +298,7 @@ app.post("/user/update", async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"))
   })
 }
