05a5fa5dc193b466cf4a1264f7aa6b3c7a87d4d2
Thigulla Chatrapathi Reddy <chatrapathireddythigulla@gmail.com>
2026-04-13 15:23:18 +0530

    Revert "client url"
    This reverts commit 26af3cedcd687872859ae09fa29f5769701aafce.

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.set("trust proxy", 1)
 /* ---------------- MIDDLEWARE ---------------- */
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173", 
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true
 }))
 
